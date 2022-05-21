b1a745ca62400b79424de2bfe0cb6853c2ec0c9e
LeonGeldsch <leon.geldschlaeger@gmail.com>
2022-05-21 23:19:37 +0200

    added mixer functionality

--- js/mixer.js
+++ js/mixer.js
@@ -1,9 +1,32 @@
 const allMixerControls = document.querySelectorAll(".mixer-control");
 const allMixerControlBounds = document.querySelectorAll(".mixer-control-bound");
+const allAudios = [
+    new Audio("./audio/order.mp3"),
+    new Audio("./audio/jazzy-abstract-beat.mp3"),
+    new Audio("./audio/voice-of-nature.mp3"),
+    new Audio("./audio/jazzy-abstract-beat.mp3"),
+    new Audio("./audio/order.mp3"),
+    new Audio("./audio/order.mp3"),
+    new Audio("./audio/order.mp3")
+]
+var allDraggableControls = [];
+
+function onDrag(dragObject) {
+    var index = dragObject.target.dataset.index;
+    var yPos = this.y;
+    var percent = yPos / -138;
+    allAudios[index].volume = percent;
+}
 
 for (let i = 0; i < allMixerControls.length; i++) {
-    Draggable.create(allMixerControls[i], {
+    allDraggableControls = [...allDraggableControls, Draggable.create(allMixerControls[i], {
         bounds: allMixerControlBounds[i],
         type: "y"
-    });
+    })];
+    allDraggableControls[i][0].addEventListener("drag", onDrag);
 }
+
+allAudios.forEach(audio => {
+    audio.play();
+    audio.volume = 0;
+});
